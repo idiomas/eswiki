e93a4377cb39312077618e752e000f875aa6715d
spencerhitch <spencerhitch@gmail.com>
2013-11-18 19:43:35 -0800

    regex prints english url

--- revolucion.py
+++ revolucion.py
@@ -1,6 +1,10 @@
-import nltk, numpy, matplotlib, urllib, string
+import nltk, numpy, urllib, string, re
 
-revolution = nltk.word_tokenize(urllib.urlopen('https://es.wikipedia.org/wiki/Revoluci%C3%B3n_mexicana').read())
+revolution = urllib.urlopen('https://es.wikipedia.org/wiki/Revoluci%C3%B3n_mexicana').read()
+m = re.search(r'(en\.wikipedia\.org/wiki/[\S^"]+)\"', revolution)
+if m:
+    print m.group(1)
+revolution = nltk.word_tokenize(revolution)
 stops = nltk.corpus.stopwords.words('spanish')
 
 tokens = [w.lower().strip(string.punctuation) for w in revolution if w.lower().strip(string.punctuation) not in stops]
@@ -8,6 +12,5 @@ tokens = [w for w in tokens if w.isalpha()]
 
 freq = (nltk.FreqDist(tokens)).keys()
 
-print(freq[:400])
 
 
