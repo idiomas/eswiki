d79d0e8ab8657464d4c2776850ea5f975b433b2d
Huda Khayrallah <hudak@berkeley.edu>
2013-12-08 11:38:23 -0800

    get difintions from babelnet

--- process_babel.py
+++ process_babel.py
@@ -2,25 +2,21 @@
 import nltk, urllib, re, string
 from urllib import urlopen
 
-raw = ""
-urls = [
-"http://babelnet.org/explore.jsp?word=amigo&lang=ES", "http://babelnet.org/explore.jsp?word=tengo&lang=ES"
-]
+def get_defintions(words):
+	raw = ""
+	urls = ["http://babelnet.org/explore.jsp?word=" + word +"&lang=ES" for word in words]
+	meanings = {}
+	for url in urls:
+		print
+    	raw = urlopen(url).read()
+    	defintions = []
+    	defintions += re.findall(r'color: #333333;">.*</a>', raw)
+    	defintions = [d[17:-4] for d in defintions]
+    	meanings[url[37:-8]]= defintions
 
-#print urls
-#re.findall(r'>.*</a>', raw)
-meanings = {}
-for url in urls:
-    raw += urlopen(url).read()
-    defintions = []
-    defintions += re.findall(r'color: #333333;">.*</a>', raw)
-    defintions = [d[17:-4] for d in defintions]
-    meanings[url[37:-8]]= defintions
+    	return meanings
 
-
-print meanings
-
-
+print get_defintions(["tener", "amigo"])
 
 
 
